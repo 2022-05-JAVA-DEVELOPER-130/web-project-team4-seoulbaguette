5693c1bff65ceb8808f06d77d401bfec8f289ce2
academyitwill <whlee0668@naver.com>
2022-08-05 16:52:58 +0900

    .

--- src/main/webapp/js/product.js
+++ src/main/webapp/js/product.js
@@ -1,3 +1,20 @@
 /**
  * 
  */
+
+function order_create_form() {
+
+
+	document.product_detail_form.cart_qty.value = document.add_cart_form.cart_qty.value;
+	document.product_detail_form.method = 'POST';
+	document.product_detail_form.action = 'order_create_form.jsp';
+	document.product_detail_form.submit();
+
+}
+function productList() {
+	location.href = 'product_list.jsp';
+}
+
+function aaa() {
+	
+}
