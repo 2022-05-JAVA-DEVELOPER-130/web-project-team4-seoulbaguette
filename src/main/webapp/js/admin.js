483889c45ace08724098bca51449e89891d87d6f
ddZoebb <jjushine24@naver.com>
2022-08-07 20:51:48 +0900

    admin product delete 완료

--- src/main/webapp/js/admin.js
+++ src/main/webapp/js/admin.js
@@ -4,35 +4,58 @@ function productInsert() {
 	document.f.submit();
 }
 
-function productCancel(){
-location.href="product_list.jsp";
+function productCancel() {
+	location.href = "product_list.jsp";
 
 }
 
-function productCancelM(){
-document.f.action = "product_detail.jsp";
+function productCancelM() {
+	document.f.action = "product_detail.jsp";
 	document.f.method = 'POST';
 	document.f.submit();
 }
 
-function productModifyForm(){
-document.product_detail_form.action="admin_product_modify_form.jsp";
-document.product_detail_form.method="post";
-document.product_detail_form.submit();
+function productModifyForm() {
+	document.product_detail_form.action = "admin_product_modify_form.jsp";
+	document.product_detail_form.method = "post";
+	document.product_detail_form.submit();
 
 }
 
-function productModify(){
-document.f.action="admin_product_modify_action.jsp";
-document.f.method="post";
-document.f.submit();
+function productModify() {
+	if (document.f.p_name.value == "") {
+		alert("상품명을 입력하십시요.");
+		f.p_name.focus();
+		return false;
+	}
+	if (document.f.p_price.value == "") {
+		alert("상품 가격을 입력하십시요.");
+		f.p_price.focus();
+		return false;
+	}
+	if (document.f.p_desc.value == "") {
+		alert("상품 설명을 입력하십시요.");
+		f.p_desc.focus();
+		return false;
+	}
+	document.f.action = "admin_product_modify_action.jsp";
+	document.f.method = "post";
+	document.f.submit();
 }
 
-function checkAdmin(id){
-if(id=="admin"){
-document.getElementById("admin_m").disabled=false;
-}else{
-document.getElementById("admin_m").disabled=true;
+function productDelete() {
+	alert("상품을 삭제하시겠습니까?");
+	document.product_detail_form.action = "admin_product_delete_action.jsp";
+	document.product_detail_form.method = "post";
+	document.product_detail_form.submit();
+
 }
+
+function checkAdmin(id) {
+	if (id == "admin") {
+		document.getElementById("admin_m").disabled = false;
+	} else {
+		document.getElementById("admin_m").disabled = true;
+	}
 
 }
